f21fe0af0c57df09a8a295f7d96da73e33806792
JWright <JWright@10.250.15.31>
2017-09-13 15:35:11 -0400

    Added Comparison class and all combinations

--- src/f17sept13/Comparisons.java
+++ src/f17sept13/Comparisons.java
@@ -8,5 +8,10 @@ public class Comparisons {
     public static void main(String[] args)
     {
         System.out.printf("5 > 10 is a %s statement%n", 5>10);
+        System.out.printf("5 < 10 is a %s statement%n", 5<10);
+        System.out.printf("5 <= 10 is a %s statement%n", 5<=10);
+        System.out.printf("5 >= 10 is a %s statement%n", 5>=10);
+        System.out.printf("5 == 10 is a %s statement%n", 5==10);
+        System.out.printf("5 == 5 is a %s statement%n", 5==5);
     }
 }
