7ece082d2ff1348c3b82623fc0dc2553220fbd49
JWright <JWright@10.250.15.31>
2017-09-13 15:26:41 -0400

    Added Comparison class

---
+++ src/f17sept13/Comparisons.java
@@ -0,0 +1,12 @@
+package f17sept13;
+
+/**
+ *
+ * @author JWright
+ */
+public class Comparisons {
+    public static void main(String[] args)
+    {
+        System.out.printf("5 > 10 is a %s statement%n", 5>10);
+    }
+}
