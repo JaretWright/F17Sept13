803b94d971580aae494fe7ec9befdbe7c1559d2e
JWright <JWright@10.250.15.31>
2017-09-13 15:46:08 -0400

    Added Average class

---
+++ src/f17sept13/Average.java
@@ -0,0 +1,33 @@
+package f17sept13;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JWright
+ */
+public class Average {
+    public static void main(String[] args)
+    {
+        //this defines 4 variables that can hold decimal numbers 
+        double num1, num2, num3, avg;
+        
+        //this defines a Scanner object called "keyboard" so we can get user input
+        Scanner keyboard = new Scanner(System.in);
+       
+        // Ask the user to enter 3 numbers
+        System.out.print("Please enter the first number: ");
+        num1 = keyboard.nextDouble();
+        
+        System.out.print("Please enter the second number: ");
+        num2 = keyboard.nextDouble();
+        
+        System.out.print("Please enter the third number: ");
+        num2 = keyboard.nextDouble();
+        
+        // The program will calculate the average of the 3 numbers
+        // The program will display the average of the 3 numbers to the screen
+
+        
+    }
+}
